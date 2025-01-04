b66bacfc5779b68ce776473ded22d876c07d556a
renuTester <renu38702@gmail.com>
2025-01-04 21:04:09 +0000

    COnfig

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,17 +13,25 @@ export default defineConfig({
 	numTestsKeptInMemory: 0,
 	screenshotsFolder: "mochawesome-report/screenshots",
 	videosFolder: "mochawesome-report/videos",
-	chromeWebSecurity: false,
 	redirectionLimit: 80,
 	reporter: "cypress-multi-reporters",
 	reporterOptions: {
 		configFile: "config.json"
 	},
 	e2e: {
+		env: {
+			firefoxOptions: {
+				args: ["-private", "-disable-web-security", "--no-sandbox"]
+			}
+		},
 		slowTestThreshold: 10000,
-		specPattern: "cypress/e2e/*/*.spec.cy.ts",
+		specPattern: "cypress/e2e/accessibility/*.spec.cy.ts",
 		testIsolation: false,
 		baseUrl: "https://todomvc.com/examples/react/dist/",
+		retries: {
+			runMode: 2,
+			openMode: 1
+		},
 		setupNodeEvents(on, config) {
 			on("before:browser:launch", (browser = {}, launchOptions) => {
 				prepareAudit(launchOptions);
