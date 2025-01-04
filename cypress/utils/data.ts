5bed438676f7e601001609e4370e494966bf2666
renuTester <renu38702@gmail.com>
2025-01-04 21:01:28 +0000

    utils

--- cypress/utils/data.ts
+++ cypress/utils/data.ts
@@ -1,20 +1,20 @@
 export const Items = Object.freeze({
-	ItemOne: 'Finish the Walk',
-	ItemTwo: 'Cook Food',
-	ItemThree: 'Feed Cat'
+	ItemOne: "Finish the Walk",
+	ItemTwo: "Cook Food",
+	ItemThree: "Feed Cat"
 });
 
 export const Filter = Object.freeze({
-	All: 'All',
-	Active: 'Active',
-	Completed: 'Completed'
+	All: "All",
+	Active: "Active",
+	Completed: "Completed"
 });
 
 export const ItemsLeft = Object.freeze({
-	Zero: '0 items left!',
-	One: '1 item left!',
-	Two: '2 items left!',
-	Three: '3 items left!'
+	Zero: "0 items left!",
+	One: "1 item left!",
+	Two: "2 items left!",
+	Three: "3 items left!"
 });
 
 export const TotalItems = Object.freeze({
@@ -25,8 +25,8 @@ export const TotalItems = Object.freeze({
 });
 
 export const Text = Object.freeze({
-	EditToDoInput: 'Edit Todo Input',
-	ClearCompleted: 'Clear completed',
-	WhatNeedsToBeDone: 'What needs to be done?',
-	DoubleClickToEditToDo: 'Double-click to edit a todo'
-})
+	EditToDoInput: "Edit Todo Input",
+	ClearCompleted: "Clear completed",
+	WhatNeedsToBeDone: "What needs to be done?",
+	DoubleClickToEditToDo: "Double-click to edit a todo"
+});
