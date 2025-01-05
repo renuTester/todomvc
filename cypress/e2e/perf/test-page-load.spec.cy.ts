cec273817b8de9d92c9665da108c09a0b3d0df3d
renuTester <renu38702@gmail.com>
2025-01-05 12:09:38 +0000

    tags

--- cypress/e2e/perf/test-page-load.spec.cy.ts
+++ cypress/e2e/perf/test-page-load.spec.cy.ts
@@ -5,7 +5,7 @@ import { PageLoad, Tags } from "../../utils/config";
 
 describe([Tags.Performance], "Todo App - Page Load", { retries: 0 }, () => {
 	context("Page load time", () => {
-		it("should be with in the permissible limit", { tags: ["smoke"] }, () => {
+		it("should be with in the permissible limit", () => {
 			cy.visit("https://todomvc.com/examples/react/dist/", {
 				onLoad: (win) => {
 					const loadTime = win.performance.timing.loadEventEnd - win.performance.timing.navigationStart;
